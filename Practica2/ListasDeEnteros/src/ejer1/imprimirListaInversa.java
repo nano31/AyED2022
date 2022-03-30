1c989e4c0f0d263559af53a0cbd9c4ec14c11616
Franco Garcia <francogarcia1331@gmail.com>
2022-03-30 17:01:44 -0600

    sigo ejer 1.5

--- Practica2/ListasDeEnteros/src/ejer1/imprimirListaInversa.java
+++ Practica2/ListasDeEnteros/src/ejer1/imprimirListaInversa.java
@@ -1,12 +1,27 @@
+package ejer1;
+
 import tp02.ejercicio1.*; 
+import java.util.Scanner;
+
+
 public class ImprimirListaInversa{
 
-    public static void imprimirInverso(){
-
+    public static void imprimirInverso(ListaDeEnterosEnlazada l){
+        int tam = l.tamanio();
+        if (!l.fin()){
+            l.elemento(tam);
+            tam--;
+            imprimirInverso(l);
+        }else{
+            System.out.println(l.elemento(tam));
+        }
     }
 
 
     public static void main (String[] args){
+        ListaDeEnterosEnlazada lista = new ListaDeEnterosEnlazada();
+        lista.comenzar();
+
 
     }
 
