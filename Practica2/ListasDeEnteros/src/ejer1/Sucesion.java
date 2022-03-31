a3f5125aec5aa2d301134d080a1a903a24ab7cde
Franco Garcia <francogarcia1331@gmail.com>
2022-03-30 21:46:34 -0600

    continuo ejer 1.6

--- Practica2/ListasDeEnteros/src/ejer1/Sucesion.java
+++ Practica2/ListasDeEnteros/src/ejer1/Sucesion.java
@@ -6,11 +6,35 @@ import java.util.Scanner;
 
 public class Sucesion{
 
-    public static void imprimirSucesion(){
+    public static Integer calcular(){
+        if (n > 1){ /** controla que el nro sea mayor estricto que 1 antes de arrancar */
+            
+            if (n % 2 == 0){
+                n = n/2;
+            }else if (n % 2 != 0){
+                n = 3*n+1;
+            }
+        }
+
+        return n;
+    }
+
+    public ListaDeEnterosEnlazada calcularSucesion(int n){
+        ListaDeEnterosEnlazada lista;
+        if (n > 1){
+           lista = calcularSucesion(calcular(n)); 
+        }else if (n < 1){
+            l = new ListaDeEnterosEnlazada();
+        }
+        lista.agregarInicio();
 
     }
 
     public static void main(String[] args){
+        int nro;
+        System.out.println("Ingrese un nro: ");
+        nro = in.nextInt();
 
+        calcularSucesion(nro);
     }
 }
