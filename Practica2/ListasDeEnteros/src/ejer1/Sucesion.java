9415390f3bcb9f8031ff2c30d1de175f27984dbd
Franco Garcia <francogarcia1331@gmail.com>
2022-03-30 21:27:05 -0600

    arranco 1.6

---
+++ Practica2/ListasDeEnteros/src/ejer1/Sucesion.java
@@ -0,0 +1,16 @@
+package ejer1;
+
+import tp02.ejercicio1.*;
+import java.util.Scanner;
+
+
+public class Sucesion{
+
+    public static void imprimirSucesion(){
+
+    }
+
+    public static void main(String[] args){
+
+    }
+}
